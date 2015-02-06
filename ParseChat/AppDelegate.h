9982adbe6d4d5e307f48f9de3c5954ca8642ace3
Tejas Lagvankar <advantej@yahoo-inc.com>
2015-02-05 17:57:40 -0800

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Tejas Lagvankar on 2/5/15.
+//  Copyright (c) 2015 Yahoo!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
